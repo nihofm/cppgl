602b2d8b32d7c43e2a1763550cde80f04cf5e74a
Nikolai Hofmann <nikolai.hofmann@fau.de>
2020-05-30 17:08:46 +0200

    anim cont, TODO pos spline, json I/O

--- src/anim.cpp
+++ src/anim.cpp
@@ -1,51 +1,41 @@
 #include "anim.h"
+#include "camera.h"
 
-Animation::Animation(const std::string& name) : NamedMap(name), t(0), ms_between_nodes(1000), running(false) {}
+Animation::Animation(const std::string& name) : NamedMap(name), time(0), ms_between_nodes(1000), running(false) {}
 
 Animation::~Animation() {}
 
-void Animation::add_node() {
-    add_node(*Camera::current());
-}
-
-void Animation::add_node(const Camera& cam) {
-    AnimNode node;
-    cam.store(node.pos, node.rot);
-    add_node(node);
-}
-
-void Animation::add_node(const AnimNode& node) {
-    nodes.push_back(node);
-}
-
-void Animation::insert_node(size_t i, const AnimNode& node) {
-    if (i < nodes.size())
-        nodes[i] = node;
+void Animation::update(float dt_ms) {
+    if (running && time < camera_path.size() && !camera_path.empty()) {
+        // eval
+        const size_t at = glm::floor(time);
+        const auto& lower = camera_path[std::min(at, camera_path.size()) % camera_path.size()];
+        const auto& upper = camera_path[std::min(at + 1, camera_path.size()) % camera_path.size()];
+        // slerp
+        const glm::vec3& pos = glm::mix(lower.first, upper.first, time - at); // TODO spline
+        const glm::quat& rot = glm::slerp(lower.second, upper.second, time - at);
+        Camera::current()->load(pos, rot);
+        // advance
+        time = glm::min(time + dt_ms / ms_between_nodes, float(camera_path.size()));
+    } else
+        running = false;
 }
 
 void Animation::clear() {
-    nodes.clear();
+    camera_path.clear();
+    data_int.clear();
+    data_float.clear();
+    data_vec2.clear();
+    data_vec3.clear();
+    data_vec4.clear();
 }
 
-size_t Animation::size() const {
-    return nodes.size();
-}
-
-void Animation::update(float dt_ms) {
-    if (running) {
-        t = std::min(t + dt_ms, nodes.size() * ms_between_nodes);
-        const size_t at = floor(t / ms_between_nodes);
-        const AnimNode& lower = nodes[std::min(at, nodes.size() - 1)];
-        const AnimNode& upper = nodes[std::min(at + 1, nodes.size() - 1)];
-        // slerp
-        const glm::vec3& pos = glm::mix(lower.pos, upper.pos, t / ms_between_nodes - at);
-        const glm::quat& rot = glm::slerp(lower.rot, upper.rot, t / ms_between_nodes - at);
-        Camera::current()->load(pos, rot);
-    }
+size_t Animation::length() const {
+    return camera_path.size();
 }
 
 void Animation::play() {
-    t = 0;
+    time = 0;
     running = true;
 }
 
@@ -53,11 +43,99 @@ void Animation::pause() {
     running = false;
 }
 
+void Animation::toggle_pause() {
+    running = !running;
+}
+
 void Animation::stop() {
-    t = 0;
+    time = 0;
     running = false;
 }
 
 void Animation::reset() {
-    t = 0;
+    time = 0;
+}
+
+size_t Animation::push_node(const glm::vec3& pos, const glm::quat& rot) {
+    const size_t i = camera_path.size();
+    camera_path.push_back(std::make_pair(pos, rot));
+    return i;
+}
+
+void Animation::put_node(size_t i, const glm::vec3& pos, const glm::quat& rot) {
+    if (i < camera_path.size())
+        camera_path[i] = std::make_pair(pos, rot);
+}
+
+void Animation::put_data(const std::string& name, size_t i, int val) {
+    if (data_int[name].size() <= i)
+        data_int[name].resize(i + 1);
+    data_int[name][i] = val;
+}
+
+void Animation::put_data(const std::string& name, size_t i, float val) {
+    if (data_float[name].size() <= i)
+        data_float[name].resize(i + 1);
+    data_float[name][i] = val;
+}
+
+void Animation::put_data(const std::string& name, size_t i, const glm::vec2& val) {
+    if (data_vec2[name].size() <= i)
+        data_vec2[name].resize(i + 1);
+    data_vec2[name][i] = val;
+}
+
+void Animation::put_data(const std::string& name, size_t i, const glm::vec3& val) {
+    if (data_vec3[name].size() <= i)
+        data_vec3[name].resize(i + 1);
+    data_vec3[name][i] = val;
+}
+
+void Animation::put_data(const std::string& name, size_t i, const glm::vec4& val) {
+    if (data_vec4[name].size() <= i)
+        data_vec4[name].resize(i + 1);
+    data_vec4[name][i] = val;
+}
+
+glm::vec3 Animation::eval_pos() const {
+    const size_t at = glm::floor(time);
+    const auto& lower = camera_path[std::min(at, camera_path.size()) % camera_path.size()];
+    const auto& upper = camera_path[std::min(at + 1, camera_path.size()) % camera_path.size()];
+    return glm::mix(lower.first, upper.first, time - at);
+}
+
+glm::quat Animation::eval_rot() const {
+    const size_t at = glm::floor(time);
+    const auto& lower = camera_path[std::min(at, camera_path.size()) % camera_path.size()];
+    const auto& upper = camera_path[std::min(at + 1, camera_path.size()) % camera_path.size()];
+    return glm::slerp(lower.second, upper.second, time - at);
+}
+
+int Animation::eval_int(const std::string& name) const {
+    const size_t i = std::min(size_t(glm::floor(time)), data_int.at(name).size() - 1);
+    return data_int.at(name).at(i); // no lerp necessary
+}
+
+// lerp helper
+template <typename T> inline T lerp_data(const std::vector<T>& data, float time) {
+    const size_t at = glm::floor(time);
+    const auto& lower = data[std::min(at, data.size()) % data.size()];
+    const auto& upper = data[std::min(at + 1, data.size()) % data.size()];
+    return glm::mix(lower, upper, time - at);
+}
+
+float Animation::eval_float(const std::string& name) const {
+    return lerp_data<float>(data_float.at(name), time);
+}
+
+glm::vec2 Animation::eval_vec2(const std::string& name) const {
+    return lerp_data<glm::vec2>(data_vec2.at(name), time);
+}
+
+glm::vec3 Animation::eval_vec3(const std::string& name) const {
+    return lerp_data<glm::vec3>(data_vec3.at(name), time);
+}
+
+glm::vec4 Animation::eval_vec4(const std::string& name) const {
+    return lerp_data<glm::vec4>(data_vec4.at(name), time);
 }
--- src/anim.h
+++ src/anim.h
@@ -1,40 +1,62 @@
 #pragma once
 
-#include <string>
+#include <map>
 #include <vector>
+#include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include "named_map.h"
-#include "camera.h"
 
-// TODO payload
-
-struct AnimNode {
-    glm::vec3 pos;
-    glm::quat rot;
-};
+// TODO import/export (json?)
 
 class Animation : public NamedMap<Animation> {
 public:
     Animation(const std::string& name);
     virtual ~Animation();
 
-    void add_node();
-    void add_node(const Camera& cam);
-    void add_node(const AnimNode& node);
-    void insert_node(size_t i, const AnimNode& node);
-    void clear();
-    size_t size() const;
-
+    // step animation and apply to Camera::current() if running
     void update(float dt_ms);
+
+    void clear();
+    size_t length() const;
 
     void play();
     void pause();
+    void toggle_pause();
     void stop();
     void reset();
 
+    // add/modify camera path nodes
+    size_t push_node(const glm::vec3& pos, const glm::quat& rot);
+    void put_node(size_t i, const glm::vec3& pos, const glm::quat& rot);
+    // insert data at given node along camera path
+    void put_data(const std::string& name, size_t i, int val);
+    void put_data(const std::string& name, size_t i, float val);
+    void put_data(const std::string& name, size_t i, const glm::vec2& val);
+    void put_data(const std::string& name, size_t i, const glm::vec3& val);
+    void put_data(const std::string& name, size_t i, const glm::vec4& val);
+
+    // evaluate
+    glm::vec3 eval_pos() const;
+    glm::quat eval_rot() const;
+    int eval_int(const std::string& name) const;
+    float eval_float(const std::string& name) const;
+    glm::vec2 eval_vec2(const std::string& name) const;
+    glm::vec3 eval_vec3(const std::string& name) const;
+    glm::vec4 eval_vec4(const std::string& name) const;
+
     // data
-    std::vector<AnimNode> nodes;
-    float t;
+    float time;
     float ms_between_nodes;
     bool running;
+    std::vector<std::pair<glm::vec3, glm::quat>> camera_path;
+    std::map<std::string, std::vector<int>> data_int;
+    std::map<std::string, std::vector<float>> data_float;
+    std::map<std::string, std::vector<glm::vec2>> data_vec2;
+    std::map<std::string, std::vector<glm::vec3>> data_vec3;
+    std::map<std::string, std::vector<glm::vec4>> data_vec4;
 };
+
+// variadic alias for std::make_shared<>(...)
+template <class... Args> std::shared_ptr<Animation> make_animation(Args&&... args) {
+    return std::make_shared<Animation>(args...);
+}

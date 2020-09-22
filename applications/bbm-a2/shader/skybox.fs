c7fb2820127ca51fa272f382ecdc6f66a9f998fa
Linus Franke <linus.franke@fau.de>
2020-09-22 15:01:24 +0200

    Merge commit '06fe042d891dc35aa8d6d928f387730a9004fa45' as 'common/cppgl'

--- applications/bbm-a2/shader/skybox.fs
+++
@@ -1,7 +0,0 @@
-#version 130
-in vec2 tc;
-out vec4 out_col;
-uniform sampler2D diffuse;
-void main() {
-    out_col = texture(diffuse, tc);
-}
--- applications/pacmap-a4/parse.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "osm.h"
-
-void setup_map(const std::string& filename);

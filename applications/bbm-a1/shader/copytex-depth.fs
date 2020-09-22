501f1bb408acec4a896f187fe80927c6c3aac19e
Linus Franke <linus.franke@fau.de>
2020-09-22 18:47:54 +0200

    rewritten cmdline, added more cmake

--- applications/bbm-a1/shader/copytex-depth.fs
+++
@@ -1,9 +0,0 @@
-#version 130
-in vec2 tc;
-out vec4 out_col;
-uniform sampler2D depth;
-uniform sampler2D col;
-void main() {
-    gl_FragDepth = texture(depth, tc).r;
-    out_col = texture(col, tc);
-}
--- applications/bbm-server/loop_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for (( ; ; ))
-do
-   ./bbm-server
-done

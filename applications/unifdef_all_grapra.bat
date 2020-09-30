001f6da3e382b36c0082f400b76d5ed4980ebdf2
Linus Franke <linus.franke@fau.de>
2020-09-30 16:34:41 +0200

    fixed unifdef script

--- applications/unifdef_all_grapra.bat
+++ applications/unifdef_all_grapra.bat
@@ -1,3 +1,4 @@
-for /r %%i in (*) do ../unifdef_windows/unifdef.exe ^
+
+for /r %%i in (*) do %~dp0..\unifdef_windows\unifdef.exe ^
  -DA1_1 -DA1_2 -DA1_3 -DA1_5 -DA1_6 -DA1_7 -DA1_8 -DA1_9 -DA1_8_ex ^
  -m %%i

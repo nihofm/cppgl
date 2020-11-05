728ecdf9ac8522d2071d0ac63b9789b43694809f
Linus Franke <linus.franke@fau.de>
2020-11-05 16:57:01 +0100

    added check for render-data folder in bbm-a1 and bbm-a2

--- applications/bbm-server/loop_server.bat
+++
@@ -1,3 +0,0 @@
-:loop
-bbm-server.exe
-goto loop
--- applications/unifdef_all_grapra.bat
+++
@@ -1,4 +0,0 @@
-
-for /r %%i in (*) do %~dp0..\unifdef_windows\unifdef.exe ^
- -DA1_1 -DA1_2 -DA1_3 -DA1_4 -DA1_5 -DA1_6 -DA1_7 -DA1_8 -DA1_9 -DA1_8_ex ^
- -m %%i
